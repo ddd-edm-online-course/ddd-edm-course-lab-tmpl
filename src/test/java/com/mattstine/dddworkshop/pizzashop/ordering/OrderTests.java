0b6fb39a1e8acf234c74c661b8a6e31b2e1c37e9
Matt Stine <mstine@pivotal.io>
2018-07-12 14:17:02 -0500

    dry up order tests

--- src/test/java/com/mattstine/dddworkshop/pizzashop/ordering/OrderTests.java
+++ src/test/java/com/mattstine/dddworkshop/pizzashop/ordering/OrderTests.java
@@ -18,25 +18,30 @@ import static org.mockito.Mockito.verify;
 public class OrderTests {
 
 	private EventLog eventLog;
+	private Order order;
+	private Pizza pizza;
+	private OrderRef ref;
 
 	@Before
 	public void setUp() {
+		ref = new OrderRef();
 		eventLog = mock(EventLog.class);
+		order = Order.builder()
+				.type(Order.Type.PICKUP)
+				.eventLog(eventLog)
+				.ref(ref)
+				.build();
+		pizza = Pizza.builder().size(Pizza.Size.MEDIUM).build();
 	}
 
 	@Test
 	public void should_create_pickup_order() {
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(new OrderRef())
-				.build();
 		assertThat(order.isPickupOrder()).isTrue();
 	}
 
 	@Test
 	public void should_create_delivery_order() {
-		Order order = Order.builder()
+		order = Order.builder()
 				.type(Order.Type.DELIVERY)
 				.eventLog(eventLog)
 				.ref(new OrderRef())
@@ -47,110 +52,59 @@ public class OrderTests {
 	@Test
 	public void submit_requires_at_least_one_pizza() {
 		assertThatIllegalStateException()
-				.isThrownBy(() -> Order.builder().type(Order.Type.PICKUP)
-						.ref(new OrderRef())
-						.eventLog(eventLog)
-						.build()
-						.submit());
+				.isThrownBy(() -> order.submit());
 	}
 
 	@Test
 	public void should_add_pizza() {
-		Pizza pizza = Pizza.builder().size(Pizza.Size.MEDIUM).build();
-
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(new OrderRef())
-				.build();
-
 		order.addPizza(pizza);
-
 		assertThat(order.getPizzas()).contains(pizza);
 	}
 
 	@Test
 	public void adding_pizza_fires_event() {
-		Pizza pizza = Pizza.builder().size(Pizza.Size.MEDIUM).build();
-
-		OrderRef ref = new OrderRef();
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(ref)
-				.build();
-
 		order.addPizza(pizza);
-
-		verify(eventLog).publish(eq(new Topic("ordering")), eq(new PizzaAddedEvent(ref, pizza)));
-
+		verify(eventLog)
+				.publish(eq(new Topic("ordering")),
+						eq(new PizzaAddedEvent(ref, pizza)));
 	}
 
 	@Test
 	public void submit_order_fires_event() {
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(new OrderRef())
-				.build();
 		order.addPizza(Pizza.builder().size(Pizza.Size.MEDIUM).build());
 		order.submit();
-
-		verify(eventLog).publish(eq(new Topic("ordering")), isA(OrderPlacedEvent.class));
+		verify(eventLog)
+				.publish(eq(new Topic("ordering")),
+						isA(OrderPlacedEvent.class));
 	}
 
 	@Test
 	public void submit_order_updates_state() {
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(new OrderRef())
-				.build();
-		order.addPizza(Pizza.builder().size(Pizza.Size.MEDIUM).build());
+		order.addPizza(pizza);
 		order.submit();
-
 		assertThat(order.isSubmitted()).isTrue();
 	}
 
 	@Test
 	public void calculates_price() {
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(new OrderRef())
-				.build();
-		order.addPizza(Pizza.builder().size(Pizza.Size.MEDIUM).build());
-
+		order.addPizza(pizza);
 		assertThat(order.calculatePrice()).isEqualTo(Pizza.Size.MEDIUM.getPrice());
 	}
 
 	@Test
 	public void mark_paid_updates_state() {
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(new OrderRef())
-				.build();
-		order.addPizza(Pizza.builder().size(Pizza.Size.MEDIUM).build());
+		order.addPizza(pizza);
 		order.submit();
 		order.markPaid();
-
 		assertThat(order.isPaid()).isTrue();
 	}
 
 	@Test
 	public void mark_paid_fires_event() {
-		Order order = Order.builder()
-				.type(Order.Type.PICKUP)
-				.eventLog(eventLog)
-				.ref(new OrderRef())
-				.build();
-		order.addPizza(Pizza.builder().size(Pizza.Size.MEDIUM).build());
+		order.addPizza(pizza);
 		verify(eventLog).publish(eq(new Topic("ordering")), isA(PizzaAddedEvent.class));
-
 		order.submit();
 		verify(eventLog).publish(eq(new Topic("ordering")), isA(OrderPlacedEvent.class));
-
 		order.markPaid();
 		verify(eventLog).publish(eq(new Topic("ordering")), isA(OrderPaidEvent.class));
 	}
