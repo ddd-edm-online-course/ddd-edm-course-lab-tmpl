71f54c514a74ba67767a6478e66eba55b48126fb
Matt Stine <matt.stine@gmail.com>
2018-07-16 19:55:42 -0600

    polish

--- src/main/java/com/mattstine/dddworkshop/pizzashop/ordering/OrderRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/ordering/OrderRepository.java
@@ -1,11 +1,12 @@
 package com.mattstine.dddworkshop.pizzashop.ordering;
 
+import com.mattstine.dddworkshop.pizzashop.infrastructure.repository.ports.Repository;
 import com.mattstine.dddworkshop.pizzashop.payments.PaymentRef;
 
 /**
  * @author Matt Stine
  */
-interface OrderRepository {
+interface OrderRepository extends Repository<OrderRef, Order, Order.OrderState, OrderEvent, OrderAddedEvent> {
     void add(Order order);
 
     OrderRef nextIdentity();
